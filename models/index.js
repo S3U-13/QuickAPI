e5039307d8d8e655e07969970c0f2a592f95a96d
unknown <ppk@ICT-DEV-NES.Leelawadee.go.th>
2025-09-12 14:50:51 +0700

    add index.js

---
+++ models/index.js
@@ -0,0 +1,12 @@
+const Sequelize = require("sequelize");
+const sequelize = require("../config/db");
+
+const db = {};
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+//import models
+
+//associations
+
+module.exports = db;
